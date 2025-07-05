0bcd789dcf87da0d5e0a8f72ec4c3bec7a3d8db7
ArvindSingh <professionalx78@gmail.com>
2025-07-05 08:35:53 +0000

    new

---
+++ 02_basics/project1/script.js
@@ -0,0 +1,37 @@
+const input = document.getElementById('todo-input')
+const addbtn = document.getElementById('add-btn')
+const list = document.getElementById('todo-list')
+const clearbtn = document.getElementById('clear-btn')
+
+addbtn.addEventListener('click',addtodo)
+clearbtn.addEventListener('click',cleartodos)
+
+function addtodo() {
+    const todoText = input.ariaValueMax.trim();
+
+    if(todoText === ""){
+        alert("Please enter a task")
+        return;
+    }
+
+    const li = document.createElement('li')
+    li.textContent = todoText;
+
+    // Toggle completion on click
+    li.addEventListener('click',()=>{
+        li.classList.toggle("completed");
+    });
+    
+    //Right click to delete
+    li.addEventListener('contextmenu',(e)=>{
+        e.preventDefault();
+        li.remove();
+    });
+    
+    list.appendChild(li);
+    input.value=""; //clear input
+
+    function cleartodos(){
+        list.innerHTML= "";
+    }
+}
